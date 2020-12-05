2494776f55cddf3214cf8b41047dced4440ba127
hanzo <enzodurandcontact@gmail.com>
2020-12-05 18:25:19 +0100

    daily update

--- docs/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"jeu"},{"l":"jeu.computer"},{"l":"jeu.controller"},{"l":"jeu.factory"},{"l":"jeu.model"},{"l":"jeu.save"},{"l":"jeu.vue"}]
